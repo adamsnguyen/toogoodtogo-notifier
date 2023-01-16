30c2095ed0786052c00ac8bf9cc3212133826f38
Adam Nguyen <avnguyen213@gmail.com>
2023-01-16 08:51:46 -0800

    update

--- toogoodtogo-notifier/main.py
+++ toogoodtogo-notifier/main.py
@@ -10,7 +10,7 @@ import shlex
 # setup
 
 ## Begin VPN
-subprocess.Popen(f'{config.vpn["vpn-script-location"]} {config.vpn["vpn-script-input"]}', shell=True, stdin=None, stdout=None, stderr=None, close_fds=True)
+subprocess.Popen(f'{config.vpn["vpn-script-location"]} {config.vpn["vpn-script-input"]}', stdin=None, stdout=None, stderr=None, close_fds=True)
 tm.sleep(15)
 
 
@@ -53,7 +53,7 @@ while True:
 
     except Exception as e:
         print("Issues with Internet Connection...")
-        subprocess.Popen(f'{config.vpn["vpn-script-location"]} {config.vpn["vpn-script-input"]}', shell=True, stdin=None, stdout=None, stderr=None, close_fds=True)
+        subprocess.Popen(f'{config.vpn["vpn-script-location"]} {config.vpn["vpn-script-input"]}', stdin=None, stdout=None, stderr=None, close_fds=True)
         print(e)
         tgtg_client = TgtgClient(email=config.tgtg['email'], access_token=config.tgtg['access_token'],
                     refresh_token=config.tgtg['refresh_token'],
