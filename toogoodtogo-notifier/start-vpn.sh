688f9d7bb07b993233edbd0fab875dcc63354458
Adam Nguyen <avnguyen213@gmail.com>
2023-01-17 03:03:55 +0000

    update

--- toogoodtogo-notifier/start-vpn.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cd /manual-connections && VPN_PROTOCOL=wireguard DISABLE_IPV6=no DIP_TOKEN=no AUTOCONNECT=true PIA_PF=true PIA_DNS=true PIA_USER=p5550118 PIA_PASS=EKYJ3GmjsU ./run_setup.sh
